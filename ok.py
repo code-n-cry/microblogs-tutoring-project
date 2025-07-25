3ea9ef3d1506d3b6d5a1a39a8c66337cb4a564b7
code-n-cry <egor.schukiv@gmail.com>
2025-07-25 12:16:28 +0300

    Изменил ok.py

--- ok.py
+++ ok.py
@@ -1,1 +1,10 @@
-print('ok')
+subjects = {"math": 25, "history": 37, "english": 99}
+
+def sort_dict(dictionary):
+    min_score = min(dictionary.values())
+    for i in dictionary:
+        if dictionary[i] == min_score:
+            return i
+
+
+print(sort_dict(subjects))
