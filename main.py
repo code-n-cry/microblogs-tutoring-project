34e07c43a5ceb983e2a22263590ca7ae81049bd7
code-n-cry <egor.schukiv@gmail.com>
2025-08-28 11:52:41 +0300

    fix changes

--- main.py
+++ main.py
@@ -166,17 +166,19 @@ def create_tag(request: Request, db: session = Depends(get_db)):
         return RedirectResponse(url='/login')
     return templates.TemplateResponse('create_tag.html', {'request': request, 'user': is_authorized})
 
-
 @app.post('/tags/create/')
-def create_tag(request: Request, name: str = Form(...), db: session = Depends(get_db)):
+def create_tag(request: Request, name:str = Form(...), db: session = Depends(get_db)):
     is_authorized = get_current_user(request.cookies.get('access_token'), db=db)
     if not is_authorized:
-        return RedirectResponse(url='/login', status_code=307)
-    tag = Tag()
-    tag.title = name
-    db.add(tag)
+        return RedirectResponse(url='/login')
+    existing_tag = db.query(Tag).filter(Tag.title == name).first()
+    if existing_tag:
+        return templates.TemplateResponse ('create_tag.html',{'request': request,'user': is_authorized,'error': 'тег c таким именем уже существует.', 'name': name})
+    new_tag = Tag(title=name)
+    db.add(new_tag)
     db.commit()
-    return RedirectResponse(url='/create_post', status_code=302)
+
+    return RedirectResponse(url='/create_post',status_code=302)
 
 
 @app.get('/profile/edit')
